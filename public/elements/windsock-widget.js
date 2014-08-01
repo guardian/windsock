0eec0708fbbcfaed64decab34a35fd4bccfac2f6
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-08-01 17:34:54 +0100

    Expose state on windsock-widget as classes

--- public/elements/windsock-widget.js
+++ public/elements/windsock-widget.js
@@ -1,3 +1,6 @@
+// FIXME: can't use ../ possibly due to a SystemJS bug?
+import {types} from 'windsock:scripts/model';
+
 // FIXME: This is a hack to ensure the windsock-data definition is
 // executed before the windsock-widget one. Otherwise, data bindings
 // don't work properly. Very ugly to do this both in the HTML and JS
@@ -10,6 +13,22 @@ Polymer('windsock-widget', {
     if (this.hasNotices() || ! collapsed) {
       this.classList.toggle('collapsed');
     }
+  },
+
+  noticesChanged: function(notices) {
+    notices = notices || [];
+
+    var classList = this.classList;
+
+    classList.toggle('has-notices', notices.length > 0);
+
+    // Update 'has-notices--{type}' class state
+    types.forEach(function(type) {
+      classList.remove('has-notices--' + type);
+    });
+    notices.forEach(function(notice) {
+      classList.add('has-notices--' + notice.data.notice.type);
+    });
   },
 
   hasNotices: function() {
