8539144dc7f557e4af78dc894436becf47f4965f
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-08-07 16:55:14 +0100

    Rather involved trickery in the build step to work for external consumers, even in the presence of RequireJS

--- src/main/resources/public/make-dist.sh
+++ src/main/resources/public/make-dist.sh
@@ -17,8 +17,25 @@ do
     node_modules/.bin/jspm bundle windsock:elements/$element $js_bundle || exit 1
 
     # prepend JS bundle
+    # Note: we could also link to it as a sync external resource to be
+    # more CSP friendly?
     echo '<script>'  >  $target
+    # FIXME: We want to setup the mapping for our libs (Reqwest); the
+    # simplest way is to inject the whole of the config, which also
+    # contains the paths mapping. That is unlikely to be what a
+    # consumer wants, but the consumer will likely override those
+    # anyway.
+    cat config.js    >> $target
+    # FIXME: Because Reqwest (an AMD module) is getting injected in as
+    # part of the js_bundle, we need to ensure that SystemJS' define()
+    # is being used instead of any other globally registered define()
+    # (typically RequireJS if used in parallel).
+    # Hopefully we can get rid of this hack once SystemJS 0.6.8 lands
+    # with support for concurrent RequireJS/SystemJS?
+    echo 'var oldDefine = window.define;'    >> $target
+    echo 'window.define = System.amdDefine;' >> $target
     cat $js_bundle   >> $target
+    echo 'window.define = oldDefine;'        >> $target
     echo '</script>' >> $target
 
     # then inject HTML bundle
