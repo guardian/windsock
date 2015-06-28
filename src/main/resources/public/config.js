34c8d73a255efef4fa9ee219c3facf1ff4ce4237
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-06-28 16:01:14 +0100

    Upgrade jspm

--- src/main/resources/public/config.js
+++ src/main/resources/public/config.js
@@ -1,21 +1,17 @@
 System.config({
+  "transpiler": "traceur",
   "paths": {
-    "windsock:*": "*.js",
-    "npm:*": "jspm_packages/npm/*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "*": "*.js"
+    "windsock/*": "*.js",
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
   }
 });
 
 System.config({
   "map": {
-    "reqwest": "github:ded/reqwest@^1.1.2"
-  }
-});
-
-System.config({
-  "versions": {
-    "github:ded/reqwest": "1.1.2"
+    "reqwest": "github:ded/reqwest@1.1.5",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
   }
 });
 
